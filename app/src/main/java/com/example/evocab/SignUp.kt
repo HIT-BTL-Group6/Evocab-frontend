1edd5582e5dccdacf57e3b72b4edd357fa9bc929
Thi <dinhthi1440@gmail.com>
2023-07-06 21:09:42 +0700

    completed sign up screen

---
+++ app/src/main/java/com/example/evocab/SignUp.kt
@@ -0,0 +1,19 @@
+package com.example.evocab
+
+import com.example.evocab.databinding.ActivitySignUpBinding
+import com.example.sourcebase.base.BaseActivity
+
+class SignUp : BaseActivity<ActivitySignUpBinding>(ActivitySignUpBinding::inflate) {
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+}
