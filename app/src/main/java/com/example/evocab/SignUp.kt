0be6304e09b8301fb4e91d66cb39b0576e5b5a42
Thi <dinhthi1440@gmail.com>
2023-07-07 23:50:39 +0700

    Implement login screen

--- app/src/main/java/com/example/evocab/SignUp.kt
+++ app/src/main/java/com/example/evocab/SignUp.kt
@@ -1,11 +1,14 @@
 package com.example.evocab
 
+import android.content.Intent
 import com.example.evocab.databinding.ActivitySignUpBinding
 import com.example.sourcebase.base.BaseActivity
 
 class SignUp : BaseActivity<ActivitySignUpBinding>(ActivitySignUpBinding::inflate) {
     override fun initData() {
-
+        binding.txtLoginChangeActi.setOnClickListener {
+            startActivity(Intent(this,LogIn::class.java))
+        }
     }
 
     override fun handleEvent() {
--- app/src/main/java/com/example/evocab/ui/IntroActivity.kt
+++ app/src/main/java/com/example/evocab/ui/IntroActivity.kt
@@ -1,17 +1,18 @@
 package com.example.evocab.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.example.evocab.MainActivity
-import com.example.evocab.R
+import com.example.evocab.LogIn
 import com.example.evocab.databinding.ActivityIntroBinding
 import com.example.sourcebase.base.BaseActivity
 
 class IntroActivity : BaseActivity<ActivityIntroBinding>(ActivityIntroBinding::inflate) {
     override fun initData() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,LogIn::class.java))
+        },2000 )
+        binding.lottie.animate().setDuration(2000).setStartDelay(0)
 
     }
 
@@ -19,17 +20,5 @@ class IntroActivity : BaseActivity<ActivityIntroBinding>(ActivityIntroBinding::i
     }
 
     override fun bindData() {
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
-        },2000 )
-
-        binding.lottie.animate().setDuration(2000).setStartDelay(0)
-//        binding.view.animate().setDuration(2000).rotationY(360f).setStartDelay(0)
-
     }
 }
