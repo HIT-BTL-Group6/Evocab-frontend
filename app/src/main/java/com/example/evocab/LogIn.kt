9e3fe770edd1b582d53f278353591d8c8cf71d44
Thi <dinhthi1440@gmail.com>
2023-07-08 13:41:26 +0700

    completed forgot password screen: enter email and verifi code

--- app/src/main/java/com/example/evocab/LogIn.kt
+++ app/src/main/java/com/example/evocab/LogIn.kt
@@ -1,18 +1,23 @@
 package com.example.evocab
 
 import android.content.Intent
+import android.view.View
 import com.example.evocab.databinding.ActivityLoginBinding
 import com.example.sourcebase.base.BaseActivity
 
 class LogIn : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
     override fun initData() {
-        binding.txtSignupChangeActi.setOnClickListener {
-            startActivity(Intent(this,SignUp::class.java))
-        }
+        binding.usernameErrorMess.setVisibility(View.GONE)
+        binding.passwErrorMess.setVisibility(View.GONE)
     }
 
     override fun handleEvent() {
-
+        binding.txtSignupChangeActi.setOnClickListener {
+            startActivity(Intent(this,SignUp::class.java))
+        }
+        binding.txtForgotPassChangeActi.setOnClickListener {
+            startActivity(Intent(this, ForgotPassword::class.java))
+        }
     }
 
     override fun bindData() {
