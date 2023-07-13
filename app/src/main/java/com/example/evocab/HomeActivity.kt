ac502302fc8d8127690b8eb0fa4be7dbb7a25fa4
Anhltn1609 <Anhsltn1234r@gmail.com>
2023-07-13 11:26:24 +0700

    nothing

---
+++ app/src/main/java/com/example/evocab/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.evocab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.evocab.databinding.ActivityHomeBinding
+import com.example.sourcebase.base.BaseActivity
+
+class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate){
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+    }
+
+    override fun bindData() {
+    }
+
+}
