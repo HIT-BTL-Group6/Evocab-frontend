ffab86be708c913d9c3b0e7beead7bdefd59d9fc
dinhthi1440 <108991843+dinhthi1440@users.noreply.github.com>
2023-07-16 23:13:07 +0700

    implement setting screen and change type edittext (#18)

---
+++ app/src/main/java/com/example/evocab/ui/setting/SettingFragment.kt
@@ -0,0 +1,70 @@
+package com.example.evocab.ui.setting
+
+import android.app.Dialog
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.evocab.R
+import com.example.evocab.databinding.FragmentSettingBinding
+import com.example.evocab.extension.openDlChangeDate
+import com.example.evocab.extension.openDlChangeEmail
+import com.example.evocab.extension.openDlChangePassword
+import com.example.evocab.extension.openDlChangeUser
+import com.example.sourcebase.base.BaseFragment
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBinding::inflate) {
+    private val contract = registerForActivityResult(ActivityResultContracts.GetContent()){
+        binding.imgAvt.setImageURI(it)
+    }
+    override val viewModel: SettingViewModel
+        get() = ViewModelProvider(this)[SettingViewModel::class.java]
+
+    override fun destroy() {
+        super.onDestroy()
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+        val dialog = context?.let { it1 -> Dialog(it1) }
+        binding.changeUser.setOnClickListener {
+            dialog?.openDlChangeUser()
+        }
+        binding.changeEmail.setOnClickListener {
+            dialog?.openDlChangeEmail()
+        }
+        binding.changePass.setOnClickListener {
+            dialog?.openDlChangePassword()
+        }
+        binding.changeDate.setOnClickListener {
+            dialog?.openDlChangeDate()
+        }
+        binding.txtLogout.setOnClickListener {
+            findNavController().navigate(R.id.action_settingFragment_to_logInFragment)
+        }
+        binding.btnSetImg.setOnClickListener{
+            contract.launch("image/*")
+        }
+        binding.btnSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                Toast.makeText(context, R.string.notify_on, Toast.LENGTH_SHORT).show()
+                buttonView.setText("Bật")
+            }else{
+                Toast.makeText(context, R.string.notify_off, Toast.LENGTH_SHORT).show()
+                buttonView.setText("Tắt")
+            }
+        }
+    }
+
+
+
+    override fun bindData() {
+
+    }
+
+
+}
